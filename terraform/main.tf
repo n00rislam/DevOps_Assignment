6fd773a8df17f32d3060cfc14561e96dc06b7a0f
Noor <147630452+n00rislam@users.noreply.github.com>
2025-04-27 23:39:36 +0500

    Add files via upload

---
+++ terraform/main.tf
@@ -0,0 +1,71 @@
+provider "aws" {
+  region = "us-east-1"
+  access_key = "AKIAQEI6UDZ7VZBADE7K"
+  secret_key = "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"
+}
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "devops-vpc"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+resource "aws_security_group" "allow_ssh_http" {
+  name        = "allow_ssh_http"
+  description = "Allow SSH and HTTP inbound traffic"
+  vpc_id      = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+ resource "aws_instance" "web_server" {
+  ami                    = "ami-084568db4383264d4" # Amazon Linux 2 AMI
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.public_subnet.id
+  key_name               = "assign"
+  vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo yum update -y
+              sudo yum install -y docker
+              sudo service docker start
+              sudo usermod -a -G docker ec2-user
+            EOF
+
+  tags = {
+    Name = "web-server"
+  }
+}
+
